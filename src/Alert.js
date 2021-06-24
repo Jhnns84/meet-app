77575f3748f12c1a7d9ea5898df238baec1efd4a
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-06-24 11:34:35 +0200

    implemented alerts

---
+++ src/Alert.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'white';
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      background: 'rgba(0, 0, 0, 0.5)',
+      margin: '20px',
+    };
+  }
+}
+
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'white';
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      background: 'rgba(255, 0, 0, 0.7)',
+      margin: '20px',
+    };
+  }
+}
+
+export { InfoAlert, ErrorAlert };
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
-    numberOfEvents: 32
+    numberOfEvents: 32, 
+    errorText: '',
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({
-      numberOfEvents: value
-    });
-    this.props.updateNumberOfEvents(event.target.value);
+    if (value <= 0) {
+      return this.setState({
+        numberOfEvents: '',
+        errorText: 'Please enter a valid number',
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value, 
+        errorText: '',
+      });
+      this.props.updateNumberOfEvents(event.target.value);
+    }
   };
 
   // handleItemClicked = (suggestion) => {
@@ -24,12 +34,12 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="NumberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
       <Form.Label className="formLabel">Choose how many events to display</Form.Label>
       <Col >
         <input 
           type="number" 
           className="NumberInput"
-          placeholder="32"
           value={this.state.numberOfEvents}
           onChange={this.handleInputChanged}
         />
