0d7fdeb5c1dae7768c2b2fa5f5a66ed94d049af9
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-06-24 16:03:32 +0200

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,15 +3,16 @@ import puppeteer from 'puppeteer';
 describe('show/hide an event details', () => {
   let browser;
   let page;
+  jest.setTimeout(30000);
   beforeAll(async () => {
-    jest.setTimeout(30000);
     browser = await puppeteer.launch();
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
   });
 
-  afterAll(() => {
+  afterAll(async() => {
+    console.log(browser);
     browser.close();
   });
 
